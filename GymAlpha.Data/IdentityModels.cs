f3299a7d58b75a05ef40818dde4fcbe7bebf7ef8
bbautista73 <bbautista73@gmail.com>
2022-02-19 17:07:54 -0600

    Edited Identity Models

--- GymAlpha.Data/IdentityModels.cs
+++ GymAlpha.Data/IdentityModels.cs
@@ -1,4 +1,6 @@
 ﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -28,6 +30,44 @@ namespace GymAlpha.Data
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        public DbSet<ExerciseType> ExerciseTypes { get; set; }
+        public DbSet<ExerciseEquipment> ExerciseEquipments { get; set; }
+        public DbSet<WorkoutType> WorkoutTypes { get; set; }
+        public DbSet<Exercise> Exercises { get; set; }
+        public DbSet<Workout> Workouts { get; set; }
+        public DbSet<Junction> Junctions { get; set; }
+        public DbSet<Log> Logs { get; set; }
+        public DbSet<Target> Targets { get; set; }
+        public DbSet<Entries> Entries { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Conventions
+                .Remove<PluralizingTableNameConvention>();
+
+            modelBuilder
+                .Configurations
+                .Add(new IdentityUserLoginConfiguration())
+                .Add(new IdentityUserRoleConfiguration());
+        }
+    }
+
+    public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+    {
+        public IdentityUserLoginConfiguration()
+        {
+            HasKey(iul => iul.UserId);
+        }
+    }
+
+    public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+    {
+        public IdentityUserRoleConfiguration()
+        {
+            HasKey(iur => iur.UserId);
         }
     }
 }
