33444b9f9cb668b0739a0909bc9eeaa6e92cb269
bbautista73 <bbautista73@gmail.com>
2022-02-19 16:37:36 -0600

    Add project files.

---
+++ GymAlpha/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GymAlpha.Startup))]
+namespace GymAlpha
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
